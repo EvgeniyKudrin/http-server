58f53d37dcd74a4451e5307810d435c01d8e0729
EvgeniyKudrin <Evgeniy9510@yandex.ru>
2023-05-25 15:53:57 +0300

    first commit

--- src/main/java/ru/netology/Main.java
+++ src/main/java/ru/netology/Main.java
@@ -3,6 +3,6 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Server server  = new Server();
-        serve2r.listen(9999);
+        server.listen(9999);
     }
 }
